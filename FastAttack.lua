548531c7a5aaadba4614405b8c988d5140224574
nhtun6283-creator <nhtun6283@gmail.com>
2026-03-09 14:05:49 +0630

    Update FastAttack.lua

--- FastAttack.lua
+++ FastAttack.lua
@@ -1,1 +1,18 @@
-https://raw.githubusercontent.com/nhtun6283-creator/BloxFruitsFastAttack/main/FastAttack.lua
+-- Blox Fruits Fast Attack
+local fast = false
+
+local gui = Instance.new("ScreenGui", game.CoreGui)
+local btn = Instance.new("TextButton", gui)
+
+btn.Size = UDim2.new(0,120,0,40)
+btn.Position = UDim2.new(0,20,0,200)
+btn.Text = "Fast Attack : OFF"
+
+btn.MouseButton1Click:Connect(function()
+    fast = not fast
+    btn.Text = fast and "Fast Attack : ON" or "Fast Attack : OFF"
+end)
+
+spawn(function()
+    while true do
+        task.wait(0.03)
